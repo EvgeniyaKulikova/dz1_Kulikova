6eda878ae2d755915ab68751c183b03e6a7407cb
Evgeniya <kulikova.ev94@gmail.com>
2023-02-02 00:00:19 +0300

    задача1

---
+++ задача2(1)/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 2(1). 
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.Write("Input your fist number a ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input your fist number b ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max = a, min = b");
+}
+else
+{
+    Console.WriteLine("max = b, min = a");
+}
